a8026343dbf783c8b9d2ea4c660e4829418b3290
Daniel-Kos <dkosmichesky@vk.com>
2025-09-17 13:34:42 +0300

    Update Task 1.py

--- Module 2/Part 1/Task 1.py
+++ Module 2/Part 1/Task 1.py
@@ -2,8 +2,10 @@ print("Введите 3 числа")
 number1 = float(input())
 number2 = float(input())
 number3 = float(input())
+
 print("Введите + для суммы\nВведите * для произведения")
 action = input("")
+
 if action == "+":
     print(number1 + number2 + number3)
 elif action == "*":
