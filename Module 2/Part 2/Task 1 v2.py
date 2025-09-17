7d2ab8bd675c9ce1c4cfdf9c4731dfad27cf55ba
Daniel-Kos <dkosmichesky@vk.com>
2025-09-17 13:57:25 +0300

    Update Task 1 v2.py

--- Module 2/Part 2/Task 1 v2.py
+++ Module 2/Part 2/Task 1 v2.py
@@ -1,9 +1,8 @@
 import calendar
-from calendar import day_name
-from calendar import setfirstweekday
 
 print("Введите число от 0 до 6")
 num = int(input())
 
 name = calendar.day_name[num]
+
 print(name)
