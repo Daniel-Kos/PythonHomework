17aa504500e3ae6eb39c2e8929f9538ed288d54a
Daniel-Kos <dkosmichesky@vk.com>
2025-10-28 23:23:44 +0300

    Update Task 3.py
    I rewrote it like a human being

--- Module 3/Part 4/Task 3.py
+++ Module 3/Part 4/Task 3.py
@@ -1,7 +1,17 @@
-start = int(input("Enter the number from which the table will begin:"))
-end = int(input("Enter the number at which the table ends:"))
+start = int(input("Enter the beginning of the range: "))
+end = int(input("Enter the end of the range: "))
 
-for i in range(start, end+1):
+max_length = 0
+for i in range(start, end + 1):
     for j in range(1, 11):
-        print(f"{i} * {j} = {i * j}", end = '\t')
-    print("" * 10)
+        eq_length = len(f"{i} * {j} = {i * j}")
+        if eq_length > max_length:
+            max_length = eq_length
+
+for i in range(start, end + 1):
+    for j in range(1, 11):
+        equation = f"{i} * {j} = {i * j}"
+        print(equation.ljust(max_length + 1), end='')
+    print()
+    if i < end:
+        print()
