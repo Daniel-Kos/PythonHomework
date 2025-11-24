3dca9927391a642ff0fe7c26c79b5d9b5a0960d7
Daniel-Kos <dkosmichesky@vk.com>
2025-11-24 15:24:18 +0300

    Add files via upload

---
+++ Test/1/Task.py
@@ -0,0 +1,17 @@
+products = {"Fruits":[("Apple", 15, 60), ("Banana", 10, 80)],
+        "Vegetables":[("Cucumber", 30, 10), ("Tomato", 10, 50)]}
+
+for category, items in products.items():
+    print(f"{category}:")
+
+    for item in items:
+        name, quantity, price = item
+        print(f"{name}: Count:{quantity}, Unite price:{price}")
+
+m_exp = max((item for sublist in products.values()for item in sublist), key=lambda x :x[2])
+
+m_cat = max(products, key=lambda k : len(products[k]))
+
+all_sum = sum(price * count for sublist in products.values() for _, count, price in sublist)
+
+print(m_exp,",",m_cat,",",all_sum)
