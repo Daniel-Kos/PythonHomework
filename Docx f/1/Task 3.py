c7ab16ed31f3ff175cfd55e46e4d0a9bf823c226
Daniel-Kos <dkosmichesky@vk.com>
2025-11-28 13:29:43 +0300

    Update Task 3.py

--- Docx f/1/Task 3.py
+++ Docx f/1/Task 3.py
@@ -1,6 +1,6 @@
 word = input()
 word_count = 0
-offers_count = 0
+offers_number = 0
 punctuation_number = 0
 
 word_list = list(word.split(" "))
@@ -10,14 +10,13 @@ punctuation_marks = ["!",",",".","'","?","-","*","«","»"]
 for w in word_list:
     word_count += 1
 
-for o in offers_list:
-    offers_count += 1
-
-if offers_count > 1:
-    offers_count -= 1
+offers_number1 = word.count('.')
+offers_number2 = word.count('!')
+offers_number3 = word.count('?')
+offers_number = offers_number1 + offers_number2 + offers_number3
 
 for w in word:
     if w in punctuation_marks:
         punctuation_number += 1
 
-print(offers_count, word_count, punctuation_number)
+print(offers_number, word_count, punctuation_number)
